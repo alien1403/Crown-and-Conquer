540067047a0352edd4c9fa6326ecb1abc9ad28cf
alien1403 <hanghicelrazvanmihai@gmail.com>
2023-10-30 23:48:55 +0200

    Add Main Menu and Pause Menu

---
+++ Assets/Scripts/EventSystemManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EventSystemManager : MonoBehaviour
+{
+    private static EventSystemManager _instance;
+    public static EventSystemManager Instance { get { return _instance; } }
+
+    private EventSystem eventSystem;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        _instance = this;
+        eventSystem = FindAnyObjectByType<EventSystem>();
+    }
+
+    public void SetEventSystemActive(bool active)
+    {
+        if (eventSystem != null)
+        {
+            eventSystem.enabled = active;
+        }
+    }
+}
